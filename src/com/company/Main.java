da6a3b8aeebbe36c1a63299f4247303e97a0a152
dongchulchoi <l77lucky@gmail.com>
2020-05-19 16:32:10 +0900

    RotateImage.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,21 +11,21 @@ public class Main {
         /*
         // TwoSum
         TwoSum twoSum = new TwoSum();
-        int[] output = twoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
+        int[] output = twoSum.twoSum(new int[]{2,7,11,15}, 9);
         for(int elem : output){
             System.out.print(elem + " ");
         }
 
         // TwoSumII
         TwoSumII twoSumII = new TwoSumII();
-        int[] output = twoSumII.twoSum(new int[]{2, 7, 11, 15}, 9);
+        int[] output = twoSumII.twoSum(new int[]{2,7,11,15}, 9);
         for (int elem : output){
             System.out.print(elem + " ");
         }
 
         // ThreeSum
         ThreeSum threeSum = new ThreeSum();
-        List<List<Integer>> output = threeSum.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        List<List<Integer>> output = threeSum.threeSum(new int[]{-1,0,1,2,-1,-4});
         for (List<Integer> list : output){
             for(int elem : list){
                 System.out.print(elem + " ");
@@ -35,7 +35,7 @@ public class Main {
 
         // ThreeSumClosest
         ThreeSumClosest threeSumClosest = new ThreeSumClosest();
-        int output = threeSumClosest.threeSumClosest(new int[]{-1, 2, 1, -4},1);
+        int output = threeSumClosest.threeSumClosest(new int[]{-1,2,1,-4},1);
         System.out.println(output);
 
         // RomanToInteger
@@ -46,12 +46,22 @@ public class Main {
         IntegerToRoman integerToRoman = new IntegerToRoman();
         System.out.println(integerToRoman.intToRoman(58));
 
+        // ProductOfArrayExceptSelf
         ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
-        int[] output = productOfArrayExceptSelf.productExceptSelf(new int[]{1, 2, 3, 4});
+        int[] output = productOfArrayExceptSelf.productExceptSelf(new int[]{1,2,3,4});
         for (int elem : output){
             System.out.print(elem + " ");
         }
-        */
 
+        // RotateImage
+        RotateImage rotateImage = new RotateImage();
+        int[][] output = rotateImage.rotate(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+        for(int[] elemArr : output){
+            for(int elemVar : elemArr){
+                System.out.printf(elemVar+" ");
+            }
+            System.out.println();
+        }
+         */
     }
 }
---
+++ src/leetcode/RotateImage.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class RotateImage {
+    public int[][] rotate(int[][] matrix) {
+        // T.C: O(N^2)
+        // S.C: O(1)
+        // transpose matrix -> reverse each row
+        int n = matrix.length;
+
+        // tranpose matrix // ***
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){ // ***
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+
+        // reverse each row // ***
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){ // reverse only the half of the array // ***
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1]; // ***
+                matrix[i][n-j-1] = tmp;
+            }
+        }
+        return matrix;
+    }
+}
