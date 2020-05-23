2f745c0b9f024f5b5d5217716a8d17c02e92377d
dongchulchoi <l77lucky@gmail.com>
2020-05-23 16:11:29 +0900

    MergeTwoSortedLists.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -131,6 +131,20 @@ public class Main {
             output = output.next;
         }
 
+        // MergeTwoSortedLists
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        ListNode output = mergeTwoSortedLists.mergeTwoLists(l1, l2);
+        while(output != null){
+            System.out.print(output.val + " ");
+            output = output.next;
+        }
          */
 
     }
