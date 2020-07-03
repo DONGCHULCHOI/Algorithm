fed7c73aaabf0eed4accfa217eb99674e6a14025
dongchulchoi <l77lucky@gmail.com>
2020-07-03 21:06:36 +0900

    Updated: MissingNumber.java

--- src/leetcode/MissingNumber.java
+++ src/leetcode/MissingNumber.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MissingNumber {
+    // the whole array is [ 0, ..., n ] and the length of the whole array is n+1 // *****
+    // the length of given array(which is input) is n // *****
+    // the legnth of given array is the last element n in the whole array // *****
     public int missingNumberHashSet(int[] nums) {
         // HashSet
         // T.C: O(n)
@@ -13,7 +16,7 @@ public class MissingNumber {
         for(int i=0; i<nums.length; i++){
             set.add(nums[i]);
         }
-        for(int i=0; i<nums.length+1; i++){
+        for(int i=0; i<nums.length+1; i++){ // *****
             if(!set.contains(i))
                 return i;
         }
@@ -23,7 +26,7 @@ public class MissingNumber {
         // Gauss - Sum of Arithmetic sequence
         // T.C: O(n)
         // S.C: O(1)
-        int expectedSum = (nums.length + 1) * nums.length / 2; // (n+1)*(0+n) / 2 // ***
+        int expectedSum = (nums.length + 1) * nums.length / 2; // (n+1)*(0+n) / 2 // *****
         int actualSum = 0;
         for(int i=0; i<nums.length; i++){
             actualSum += nums[i];
@@ -31,3 +34,4 @@ public class MissingNumber {
         return expectedSum - actualSum;
     }
 }
+
