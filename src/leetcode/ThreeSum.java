07c81cc929483c815a828d6056fc765e621e7c03
dongchulchoi <l77lucky@gmail.com>
2020-07-04 22:53:49 +0900

    Updated: TwoSumII.java ThreeSum.java

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -8,13 +8,15 @@ public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         // T.C: O(n^2)
         // S.C: O(log n) if quick / O(n) if merge
-        // use Two Sum II
-        // should be sorted
-        // diff b\w SumTwoII: not only one solution
+        // use Two Sum II // *****
+        // should be sorted // *****
+        // If the current value is greater than zero, break from the loop. Remaining values cannot sum to zero // *****
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i < nums.length && nums[i] <= 0; i++){ // ***
-            if(i == 0 || nums[i] != nums[i-1]){ // *** because not only one solution
+        for(int i = 0; i < nums.length && nums[i] <= 0; i++){ // ***** // if nums[i] > 0, then the sum cannot be zero -> for the performance
+            if(i == 0 || nums[i] != nums[i-1]){ // *****
+                // prevent the repetition, e.g. 0 0 -1 1
+                // i == 0 is required to prevent out of index exception, and also trigger the first case of i = 0
                 twoSumII(nums, i, res);
             }
         }
@@ -26,14 +28,14 @@ public class ThreeSum {
         int hi = nums.length - 1;
         while(lo < hi){
             int sum = nums[i] + nums[lo] + nums[hi];
-            if(sum < 0 || (lo > i+1 && nums[lo] == nums[lo-1])){ // *** because not only one solution
+            if(sum < 0 || (lo > i+1 && nums[lo] == nums[lo-1])){ // ***** // right part condition occurs when sum is 0 // prevent the repeition // e.g -2 0 0 2 2
                 lo++;
             }
-            else if(sum > 0 || (hi < nums.length-1 && nums[hi] == nums[hi+1])){ // *** because not only one solution
+            else if(sum > 0){ // hi < nums.length-1 && nums[hi] == nums[hi+1]) -> don't have to deal with it, because the above did it // *****
                 hi--;
             }
-            else{
-                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--])); // Arrays.asList's T.C: O(1)
+            else{ // compared to Two Sum II, it should be located here because there is no return, which triggers Time Exceeded
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--])); // *****
             }
         }
     }
