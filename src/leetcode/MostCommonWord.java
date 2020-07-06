2fdc096762651b09b3b0ef066d3b69a169119995
dongchulchoi <l77lucky@gmail.com>
2020-07-06 20:50:46 +0900

    Updated: MostCommonWord.java ReorderDataInLogFiles.java

--- src/leetcode/MostCommonWord.java
+++ src/leetcode/MostCommonWord.java
@@ -7,41 +7,38 @@ import java.util.Set;
 
 public class MostCommonWord {
     public String mostCommonWord(String paragraph, String[] banned) {
-        // this does not pass the amazon similar question // *******************
-        // consider it again next time
-
         // T.C: O(P+B) where P is the size of paragraph and B is the size of banned
         // S.C: O(P+B) to store the count and the banned set // ***
         // assume the following:
         // Words in the list of banned words are given in lowercase, and free of punctuation.
         // Words in the paragraph are not case sensitive.  The answer is in lowercase
-        paragraph += "."; // for the case of only cosisted word with letter. e.g. paragraph = "Bob" // ***
+        // Words only consist of letters
+        paragraph += "."; // ***** // for the case of only cosisted word with letter. e.g. paragraph = "Bob"
 
-        Set<String> banset = new HashSet<>(); // *****
+        Set<String> banset = new HashSet<>();
         for(String word : banned){
             banset.add(word);
         }
 
-        Map<String, Integer> count = new HashMap<>(); // *****
-
+        Map<String, Integer> count = new HashMap<>();
         String ans = ""; // *****
-        int ansFrequency = 0; // *****
+        int maxFreq = 0; // *****
 
-        StringBuilder word = new StringBuilder(); // ***
+        StringBuilder word = new StringBuilder();
         for(char c : paragraph.toCharArray()){ // *****
             if(Character.isLetter(c)){ // *****
                 word.append(Character.toLowerCase(c));
             }
             else if(word.length()>0){ // *****
                 String finalword = word.toString();
-                if(!banset.contains(finalword)){
+                if(!banset.contains(finalword)){ // *****
                     count.put(finalword, count.getOrDefault(finalword, 0) + 1);
-                    if(count.get(finalword) > ansFrequency){ // update the most frequent word // *****
+                    if(count.get(finalword) > maxFreq){ // update the most frequent word // *****
                         ans = finalword;
-                        ansFrequency = count.get(finalword);
+                        maxFreq = count.get(finalword);
                     }
                 }
-                word = new StringBuilder();
+                word = new StringBuilder(); // *****
             }
         }
         return ans;
--- src/leetcode/ReorderDataInLogFiles.java
+++ src/leetcode/ReorderDataInLogFiles.java
@@ -4,35 +4,33 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class ReorderDataInLogFiles {
-    // Consider lambda expression next time
+    // T.C: O(n log n), where N is the total content of logs
+    // S.C: O(n) because Collections.sort() uses merge sort
     public String[] reorderLogFiles(String[] logs) {
-        // T.C: O(NlogN), where N is the total content of logs
-        // S.C: O(log n) if quick / O(n) if merge
-        Arrays.sort(logs, new CustomCompare()); // ***
-        return logs;
-    }
-}
+        Arrays.sort(logs, new Comparator<String>(){
+            public int compare(String log1, String log2)  // *****
+            {
+                String[] split1 = log1.split(" ", 2); // split the logs based on identifier // the grammer of split // ***
+                String[] split2 = log2.split(" ", 2);
+                boolean isDigit1 = Character.isDigit(split1[1].charAt(0)); // go over whether digit log or letter log // *****
+                boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
 
-class CustomCompare implements Comparator<String> // ***
-{
-    public int compare(String log1, String log2)  // ***
-    {
-        String[] split1 = log1.split(" ", 2); // extract the identifier
-        String[] split2 = log2.split(" ", 2);
-        boolean isDigit1 = Character.isDigit(split1[1].charAt(0)); // go over whether digit log or letter log // *****
-        boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
-
-        if (!isDigit1 && !isDigit2) { // isDigit1 is false, isDigit2 is false == both are letter log // *****
-            int cmp = split1[1].compareTo(split2[1]);
-            if (cmp != 0){ // The letter-logs are ordered lexicographically ignoring identifier
-                return cmp;
+                if (!isDigit1 && !isDigit2) { // isDigit1 is false, isDigit2 is false == both are letter log // *****
+                    int cmp = split1[1].compareTo(split2[1]); // The letter-logs are ordered lexicographically ignoring identifier
+                    if (cmp != 0)
+                        return cmp;
+                    else // with the identifier used in case of ties
+                        return split1[0].compareTo(split2[0]);
+                }
+                return isDigit1 ? (isDigit2 ? 0 : 1) : -1; // *1) // *****
             }
-            return split1[0].compareTo(split2[0]); // with the identifier used in case of ties
-        }
-        return isDigit1 ? (isDigit2 ? 0 : 1) : -1; // *1) // *****
+        });
+        return logs;
     }
     // 1)
     // isDigit1 is true, isDigit2 is true -> 0, The digit-logs should be put in their original order
     // isDigit1 is true, isDigit2 is false -> 1, The letter logs comes in front of the digit logs
     // isDigit1 is false, isDigit2 is true -> -1, The digit-logs should be put in their original order
+    // isDigit1 is false, isDigit2 is false -> dealt with the before
 }
+
