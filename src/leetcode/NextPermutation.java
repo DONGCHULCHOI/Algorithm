3ac068184c50e395405584071dcbab062347880e
dongchulchoi <l77lucky@gmail.com>
2020-07-16 17:09:51 +0900

    Updated: GroupAnagrams.java NextPermutation.java

--- src/leetcode/NextPermutation.java
+++ src/leetcode/NextPermutation.java
@@ -1,28 +1,23 @@
 package leetcode;
 
 public class NextPermutation {
-    public int[] nextPermutation(int[] nums) {
+    public void nextPermutation(int[] nums) {
         // T.C: O(N)
         // S.C: O(1)
-        // From the end, find the first decreasing elem, a[i]
-        // swap this with the smallest one larger than this, a[j]
-        // reverse the number from a[i+1] to a[nums.length-1]
+        // From the end, find the first decreasing elem, a[i] // *****
+        // swap this with the smallest one larger than this, a[j] // *****
+        // reverse the number from a[i+1] to a[nums.length-1] // *****
         int i = nums.length - 2;
-
-        while(i >= 0 && nums[i+1] <= nums[i]){
-            i--;
-        }
+        while(i >= 0 && nums[i] >= nums[i+1])   i--; // ***** // if i == -1 -> rearrange it as the lowest possible order at (A) // order *****
 
         if(i >= 0){
-            int j = nums.length - 1;
-            while(j >= 0 && nums[j] <= nums[i]){
+            int j = nums.length - 1; // to find j starting from the end because lexicographically sorted increasingly // *****
+            while(nums[j] <= nums[i]){
                 j--;
             }
-            swap(nums, i, j);
+            swap(nums, i, j); // *****
         }
-        reverse(nums, i+1);
-
-        return nums;
+        reverse(nums, i+1); // (A) // *****
     }
 
     public void swap(int[] nums, int i, int j){
