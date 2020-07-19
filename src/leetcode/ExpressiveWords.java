411dd88a8b3cff8abc0ba073eba2e0d2229857a3
dongchulchoi <l77lucky@gmail.com>
2020-07-19 20:14:06 +0900

    Updated: ExpressiveWords.java MaximizeDistanceToClosestPerson.java

--- src/leetcode/ExpressiveWords.java
+++ src/leetcode/ExpressiveWords.java
@@ -7,11 +7,13 @@ public class ExpressiveWords {
     public int expressiveWords(String S, String[] words) {
         // T.C: O(QK) where Q is the length of words, and K is the maximum length of a word
         // S.C: O(K)
-        // First do run length encoding which is getting the count of each charcter in string
-        // let's say we have individual counts c1 = S.count[i] and c2 = word.count[i]
-        // If c1 < c2, then we can't make the ith group of word equal to the ith word of S by adding characters
-        // If c1 >= 3, then we can add letters to the ith group of word to match the ith group of S by extending
-        // if c1 < 3, then we must have c2 == c1 for the ith groups of word and S to match
+        // extension operation: choose a group consisting of characters c,
+        // and add some number of characters c to the group so that the size of the group is 3 or more
+        // 1st. do RLE(Run Length Encoding) which is getting the count of each charcter in string
+        // 2nd. let's say we have individual counts c1 = S.count[i] and c2 = word.count[i] where word is in words
+        //      If c1 < c2, then we can't make the ith group of word equal to the ith word of S by adding characters
+        //      If c1 >= 3, then we can add letters to the ith group of word to match the ith group of S by extending
+        //          if c1 < 3, then we must have c2 == c1 for the ith groups of word and S to match
         RLE R1 = new RLE(S);
         int ans = 0;
 
@@ -21,7 +23,7 @@ public class ExpressiveWords {
             for (int i = 0; i < R1.counts.size(); ++i) {
                 int c1 = R1.counts.get(i);
                 int c2 = R2.counts.get(i);
-                if (c1 < 3 && c1 != c2 || c1 < c2)
+                if (c1 < c2 || c1 < 3 && c1 != c2)
                     continue search;
             }
             ans++;
@@ -32,20 +34,20 @@ public class ExpressiveWords {
 }
 
 class RLE { // Run Length Encoding
-    String key;
-    List<Integer> counts;
+    String key; // key
+    List<Integer> counts; // value
 
-    public RLE(String S) {
+    public RLE(String s) {
         StringBuilder sb = new StringBuilder();
         counts = new ArrayList();
 
-        char[] ca = S.toCharArray();
-        int N = ca.length;
-        int prev = -1;
-        for (int i = 0; i < N; ++i) {
-            if (i == N-1 || ca[i] != ca[i+1]) {
-                sb.append(ca[i]);
-                counts.add(i - prev);
+        char[] c = s.toCharArray();
+        int prev = -1; // *****
+
+        for (int i = 0; i < c.length; i++) {
+            if (i == c.length-1 || c[i] != c[i+1]) { // order *****
+                sb.append(c[i]); // key
+                counts.add(i - prev); // length
                 prev = i;
             }
         }
--- src/leetcode/MaximizeDistanceToClosestPerson.java
+++ src/leetcode/MaximizeDistanceToClosestPerson.java
@@ -3,13 +3,13 @@ package leetcode;
 public class MaximizeDistanceToClosestPerson {
     // Consider understanding this solution fully again later
     public int maxDistToClosest(int[] seats) {
-        // As iterate, update the closest person sitting to our left, and closest person sitting to our right
-        // Then at seat i, the closest person is min(i - left, right - i)
-        // with one exception. i - left should be considered infinite if there is no person to the left of seat i,
-        // right - i is infinite if there is no one to the right of seat i
-        // => choose min(left closest, right closest) at i -> keep updating max
         // T.C: O(N) where N is the length of seats
         // S.C: O(1)
+        // As iterate, update the closest person sitting to our left, and the closest person sitting to our right
+        // Then at seat i, the closest person is min(i - left, right - i)
+        // And update Max Distance
+        // (!) With the exceptions. When i is at 0 or length-1, it means there is no person sitting to left/right
+        //     in this case, should consider the distance of left/right infinite
         int N = seats.length;
         int left = -1, right = 0; // *****
         int res = 0;
