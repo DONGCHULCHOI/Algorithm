85ab41dacee5ec1f7584dc13f2609a8097435dfe
dongchulchoi <l77lucky@gmail.com>
2020-07-27 22:17:10 +0900

    Updated: JewelsAndStones.java ReverseInteger.java

--- src/leetcode/JewelsAndStones.java
+++ src/leetcode/JewelsAndStones.java
@@ -7,7 +7,7 @@ public class JewelsAndStones {
     public int numJewelsInStones(String J, String S) {
         // O(J + S) where J is the length of J, S is the length of S
         // O(J)
-        Set<Character> types = new HashSet();
+        Set<Character> types = new HashSet<>();
         for(char j : J.toCharArray())
             types.add(j);
 
--- src/leetcode/ReverseInteger.java
+++ src/leetcode/ReverseInteger.java
@@ -6,16 +6,16 @@ public class ReverseInteger {
         // S.C: O(1)
         int res = 0;
         while(x != 0){
-            // pop // e.g. stack looks like head -> 3 -> 2 -> 1
+            // pop // e.g. stack looks like 1 <- 2 <- 3 <- head
             int pop = x % 10;
             x /= 10;
 
             if(res > Integer.MAX_VALUE/10 || res == Integer.MAX_VALUE/10 && pop > 7) // overflow // *** // r.f. Integer.MAX_VALUE = 2147483647
-                return  0;
+                return 0;
             if(res < Integer.MIN_VALUE/10 || res == Integer.MIN_VALUE/10 && pop < -8) // underflow // *** // r.f. Integer.MIN_VALUE = -2147483648
                 return 0;
 
-            // push // e.g. stack looks like head -> 1 -> 2 -> 3
+            // push // e.g. stack looks like 3 <- 2 <- 1 <- head
             res = res * 10 + pop;
         }
         return res;
